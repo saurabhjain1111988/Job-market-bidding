41d82f847f89ed1cee3abcebcbce8f69cc0dcf9c
Saurabh Jain <saurabh_jain2@intuit.com>
2018-02-13 01:36:02 -0600

    Refactoring

--- src/main/java/com/market/jobmarket/project/dao/BaseDao.java
+++ src/main/java/com/market/jobmarket/project/dao/BaseDao.java
@@ -7,10 +7,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SocketOptions;
+import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+import com.datastax.driver.core.policies.DowngradingConsistencyRetryPolicy;
+import com.datastax.driver.core.policies.LoggingRetryPolicy;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
 import com.datastax.driver.core.BoundStatement;
 
+@Component
 public class BaseDao {
 
 	// Reusing Purpose
@@ -19,10 +29,20 @@ public class BaseDao {
 	// Application settings
 	protected static String server_ip = "127.0.0.1";
 	protected static String keyspace = "sampleDB";
+	protected static String node = "testnode.corp.intuit.net";
+	protected static String datacenter = "Intuit-data-center";
+	protected static String userName = "userName";
+	protected static String password = "password";
 
 	// Application connection objects
 	protected static Cluster cluster = null;
 	protected static Session session = null;
+
+	@PostConstruct
+	public void connect() {
+		updateCluster();
+		session = cluster.connect(keyspace);
+	}
 
 	/**
 	 * Builds Insert / Update statements for a given table and columns
@@ -59,15 +79,15 @@ public class BaseDao {
 		}
 		return where.getQueryString();
 	}
-	
+
 	/**
 	 * Builds CQL statements to select from a given table, filtered by keys
 	 *
 	 * @param table
 	 * @return Query String
 	 */
-	protected String getSelectAll(String table){
-		 return QueryBuilder.select().all().from(keyspace, table).getQueryString();
+	protected String getSelectAll(String table) {
+		return QueryBuilder.select().all().from(keyspace, table).getQueryString();
 	}
 
 	/**
@@ -108,6 +128,23 @@ public class BaseDao {
 			throw new Exception("A valid non-null BoundStatement is required.");
 		}
 		return session.execute(boundStatement);
+	}
+
+	public void updateCluster() {
+		if (null == cluster) {
+			// connect to cluster
+			Cluster.Builder cb = Cluster.builder();
+			cb.addContactPoint(node);
+			cb.withLoadBalancingPolicy(DCAwareRoundRobinPolicy.builder().withLocalDc(datacenter).build());
+			cb.withRetryPolicy(new LoggingRetryPolicy(DowngradingConsistencyRetryPolicy.INSTANCE));
+			cluster = cb.withSocketOptions(getSocketOptions()).build();
+		}
+	}
+
+	private SocketOptions getSocketOptions() {
+		SocketOptions so = new SocketOptions();
+		so.setKeepAlive(true);
+		return so;
 	}
 
 	public static void openConnection() {
